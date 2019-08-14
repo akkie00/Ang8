8a49a2df7c2b269cfbb6c1b13df0e2235c8039c7
akkie00 <akshay8801.ak@gmail.com>
2019-08-14 11:19:33 +0530

    started ang 8 app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    ServerComponent,
+    AppComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
